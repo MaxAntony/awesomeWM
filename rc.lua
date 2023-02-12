7c9eb9e487e8ee839641f198b44677ca624d1f7c
MaxAntony <maxpacami@gmail.com>
2023-02-12 14:51:08 -0500

    feat: actualizado el estilo del tasklist

--- rc.lua
+++ rc.lua
@@ -237,6 +237,55 @@ awful.screen.connect_for_each_screen(function(s)
     screen = s,
     filter = awful.widget.tasklist.filter.currenttags,
     buttons = tasklist_buttons,
+    style = {
+      shape_border_width = 1,
+      shape_border_color = '#777777',
+      shape = gears.shape.rounded_bar,
+    },
+    layout = {
+      spacing = 10,
+      spacing_widget = {
+        {
+          forced_width = 5,
+          shape = gears.shape.circle,
+          widget = wibox.widget.separator,
+        },
+        valign = 'center',
+        halign = 'center',
+        widget = wibox.container.place,
+      },
+      layout = wibox.layout.flex.horizontal,
+    },
+
+    widget_template = {
+      {
+        {
+          {
+            {
+              {
+                id = 'icon_role',
+                widget = wibox.widget.imagebox,
+              },
+              margins = 2,
+              widget = wibox.container.margin,
+            },
+            {
+              id = 'text_role',
+              widget = wibox.widget.textbox,
+            },
+            widget = wibox.layout.fixed.horizontal,
+          },
+          widget = wibox.container.place,
+          valign = 'center',
+          halign = 'center',
+        },
+        left = 10,
+        right = 10,
+        widget = wibox.container.margin,
+      },
+      id = 'background_role',
+      widget = wibox.container.background,
+    },
   })
 
   -- Create the wibox
