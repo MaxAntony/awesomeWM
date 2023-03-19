049c46f5400a293f5a520a109b0b1188aec0986f
MaxAntony <maxpacami@gmail.com>
2023-03-19 14:51:31 -0500

    agregado autrun

---
+++ autorun.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+function run {
+    if ! pgrep $1 > /dev/null ;
+    then
+        $@&
+    fi
+}
+
+setxkbmap -layout "us,es" -option "grp:alt_shift_toggle"
+
+run nitrogen --restore &
+run picom -b 
