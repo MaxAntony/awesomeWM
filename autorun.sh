dea15abfe8f12dc2441d79a5a65347f47fbab327
MaxAntony <maxpacami@gmail.com>
2023-06-13 12:44:46 -0500

    feat: add gnome polkit and move keyboard shortcut to bottom

--- autorun.sh
+++ autorun.sh
@@ -7,7 +7,10 @@ function run {
     fi
 }
 
+run nitrogen --restore
+
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+# run picom -b 
+
 setxkbmap -layout "us,es" -option "grp:alt_shift_toggle"
 
-run nitrogen --restore
-# run picom -b 
