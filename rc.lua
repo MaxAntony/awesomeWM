e0b2f140eeea4ae48fe2660f101278209e3b09de
MaxAntony <maxpacami@gmail.com>
2023-08-06 15:04:58 -0500

    feat: set theme path for beautify to ./themes/custom

--- rc.lua
+++ rc.lua
@@ -38,7 +38,7 @@ naughty.connect_signal(
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. 'default/theme.lua')
+beautiful.init(string.format('%s/.config/awesome/themes/%s/theme.lua', os.getenv('HOME'), 'custom') .. 'zenburn/theme.lua')
 
 -- This is used later as the default terminal and editor to run.
 terminal = 'kitty'
