bd3f88193e3fb95be36b81afd58c453a572fe1d9
MaxAntony <maxpacami@gmail.com>
2023-08-12 13:23:17 -0500

    :sparkles: Feat: add new programs to launch on startup

--- autorun.sh
+++ autorun.sh
@@ -11,6 +11,9 @@ run nitrogen --restore
 
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 # run picom -b 
+# Applications
+run thunderbird
+run ferdium
 
 setxkbmap -layout "us,es" -option "grp:alt_shift_toggle"
 
